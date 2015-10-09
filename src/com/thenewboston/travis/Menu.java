570d96212ddac6671f0817addb5963ffcba90237
bharat226g <bharat226g@debian>
2015-10-09 08:15:39 +0530

    textplay

--- src/com/thenewboston/travis/Menu.java
+++ src/com/thenewboston/travis/Menu.java
@@ -9,7 +9,7 @@ import android.widget.ListView;
 
 public class Menu extends ListActivity {
 	
-	String classes[] = {"startingPoint", "example1", "example2", "example3", "example4", "example5"};
+	String classes[] = {"startingPoint", "TextPlay", "example2", "example3", "example4", "example5"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
--- src/com/thenewboston/travis/Splash.java
+++ src/com/thenewboston/travis/Splash.java
@@ -21,7 +21,7 @@ public class Splash extends Activity {
 		Thread timer = new Thread(){
 			public void run(){
 				try{
-					sleep(4000);
+					sleep(2000);
 				} catch (InterruptedException e){
 					e.printStackTrace();
 				} finally{
